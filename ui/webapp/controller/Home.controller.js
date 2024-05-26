d4923893d0b90eca25af6d0428e1fe8851964bd3
Dobromir Zahariev <dobromir.zahariev@sap.com>
2024-05-26 13:00:09 +0300

    Create Task

--- ui/webapp/controller/Home.controller.js
+++ ui/webapp/controller/Home.controller.js
@@ -2,9 +2,15 @@ sap.ui.define([
 	'./BaseController',
 	'sap/ui/model/json/JSONModel',
 	'sap/ui/Device',
+	"sap/m/MessageBox",
+	"sap/m/library",
 	'com/zahariev/taskboard/model/formatter'
-], function (BaseController, JSONModel, Device, formatter) {
+], function (BaseController, JSONModel, Device, MessageBox, mobileLibrary, formatter) {
 	"use strict";
+
+	// shortcut for sap.m.ButtonType
+	var ButtonType = mobileLibrary.ButtonType;
+
 	return BaseController.extend("com.zahariev.taskboard.controller.Home", {
 		formatter: formatter,
 
@@ -29,7 +35,7 @@ sap.ui.define([
 
 			// Define task model
 			this.taskModel = new JSONModel({
-				tasks: null,
+				tasks: [],
 			})
 			this.taskModel.setSizeLimit(Number.MAX_VALUE);
 			this.setModel(this.taskModel, "tasks");
@@ -77,27 +83,96 @@ sap.ui.define([
 		},
 
 		create: async function () {
-			alert("Create New")
+			const token = await this.getOwnerComponent().getToken();
+			const url = '/api/task'
+			if (!this.oCreateDialog) {
+				this.oCreateDialog = new sap.m.Dialog({
+					title: "Create new Task",
+					content: [
+						new sap.m.Label({text:"Task Kind:"}),
+						new sap.m.ComboBox({
+							selectedKey: "handbrake",
+							items: [
+								new sap.ui.core.Item({key: "handbrake", text: "handbrake"}), 
+								new sap.ui.core.Item({key: "backup", text: "backup"})
+							], 
+							id: "KIND_COMBOBOX"
+						}),
+						new sap.m.Label({text:"From:"}),
+						new sap.m.Input({maxLength: 150, id: "FROM_INPUT"}),
+						new sap.m.Label({text:"To:"}),
+						new sap.m.Input({maxLength: 150, id: "TO_INPUT"}),
+						new sap.m.Label({text:"Additional property:"}),
+						new sap.m.Input({maxLength: 50, id: "ADDITIONAL_INPUT"})
+					],
+					beginButton: new sap.m.Button({
+						type: ButtonType.Emphasized,
+						text: "Create",
+						press: function () {
+							const kindFieldValue = sap.ui.getCore().byId("KIND_COMBOBOX").getSelectedItem().getText();
+							const fromFieldValue = sap.ui.getCore().byId("FROM_INPUT").getValue();
+							const toFieldValue = sap.ui.getCore().byId("TO_INPUT").getValue();
+							const additionalFieldValue = sap.ui.getCore().byId("ADDITIONAL_INPUT").getValue();
+							jQuery.ajax({
+								url: url,
+								type: "POST",
+								contentType: 'application/json',
+								data: JSON.stringify({
+										"status": "new",
+										"progress": "0",
+										"kind": kindFieldValue,
+										"properties": {
+											"from": fromFieldValue, 
+											"to": toFieldValue, 
+											"additional": additionalFieldValue
+										}
+								 }),
+								beforeSend: function (xhr) {
+									xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+								},
+								async:false
+							});
+							this.oCreateDialog.close();
+							this.loadData();	
+						}.bind(this)
+					}),
+					endButton: new sap.m.Button({
+						text: "Close",
+						press: function () {
+							this.oCreateDialog.close();
+						}.bind(this)
+					})
+				});
+
+				// to get access to the controller's model
+				this.getView().addDependent(this.oCreateDialog);
+			}
+
+			this.oCreateDialog.open();
 		},
 
 		delete: async function (oEvent) {
-			const task = oEvent
-					.getSource()
-					.getBindingContext("tasks")
-					.getObject();
-			if (confirm("Delete " + task.id + "?") == true) {
-				const token = await this.getOwnerComponent().getToken()
-				const url = '/api/task/'+ task.id
-				jQuery.ajax({
-					url: url,
-					type: "DELETE",
-					beforeSend: function (xhr) {
-						xhr.setRequestHeader('Authorization', `Bearer ${token}`);
-					},
-					async:false
-				});
-				this.loadData();
-			}
+			const task = oEvent.getSource().getBindingContext("tasks").getObject();
+			const token = await this.getOwnerComponent().getToken();
+			const url = '/api/task/'+ task.id
+			MessageBox.confirm("Delete " + task.id + "?", {
+				actions: [MessageBox.Action.CANCEL, "Delete"],
+				emphasizedAction: MessageBox.Action.CANCEL,
+				onClose: function (sAction) {
+					if (sAction === "Delete") {
+						jQuery.ajax({
+							url: url,
+							type: "DELETE",
+							beforeSend: function (xhr) {
+								xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+							},
+							async:false
+						});
+						this.loadData();
+					}
+				}.bind(this),
+				dependentOn: this.getView()
+			});		
 		},
 
 		loadData: async function () {
